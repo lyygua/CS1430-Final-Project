2524e0dda3903d0cd5e9bfc5ef1cec3397c7d9d4
Jiachen Wang <jiachen_wang1@brown.edu>
2024-05-11 23:32:22 -0400

    attack_fia

--- transferattack/__init__.py
+++ transferattack/__init__.py
@@ -2,12 +2,15 @@ from .gradient.fgsm import FGSM
 from .gradient.mifgsm import MIFGSM
 
 from .input_transformation.ssm import SSM
+from .advanced_objective.fia import FIA
 
 attack_zoo = {
     'fgsm': FGSM,
     'mifgsm': MIFGSM,
 
     'ssm': SSM,
+
+    'fia': FIA,
 }
 
 __version__ = '1.0.0'
